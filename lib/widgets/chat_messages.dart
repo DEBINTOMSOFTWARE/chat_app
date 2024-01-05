f98dd6a2f9e09c256b5ee867f4e6d369c608bd3f
Debin Tom <debintom.software@gmail.com>
2024-01-05 17:24:36 +0000

    loading and display chat message as a stream

--- lib/widgets/chat_messages.dart
+++ lib/widgets/chat_messages.dart
@@ -1,3 +1,5 @@
+
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
 class ChatMessages extends StatelessWidget {
@@ -5,8 +7,43 @@ class ChatMessages extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text('No messages found.'),
-    );
+    return StreamBuilder(
+        stream: FirebaseFirestore.instance.collection('chat').orderBy(
+          'createdAt',
+           descending: true
+        ).snapshots(),
+        builder: (context, chatSnapshots) {
+          if (chatSnapshots.connectionState == ConnectionState.waiting) {
+            return const Center(
+              child: CircularProgressIndicator(),
+            );
+          }
+
+          if (!chatSnapshots.hasData || chatSnapshots.data!.docs.isEmpty) {
+            return const Center(
+              child: Text('No messages found.'),
+            );
+          }
+
+          if (chatSnapshots.hasError) {
+            return const Center(
+              child: Text('Something went wrong'),
+            );
+          }
+
+          final loadedMessages = chatSnapshots.data!.docs;
+
+          return ListView.builder(
+            padding: const EdgeInsets.only(
+              bottom: 40,
+              left: 12,
+              right: 12
+            ),
+            reverse: true,
+              itemCount: loadedMessages.length,
+              itemBuilder: (context, index) {
+                return Text(loadedMessages[index].data()['text']);
+              });
+        });
   }
 }
